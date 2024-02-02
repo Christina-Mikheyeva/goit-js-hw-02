1c303569c0a051ff97a84a0bde963e5bc2b5d1af
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-02-02 19:57:01 +0100

    Forth task done

--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,24 @@
-function checkForName(fullName, firstName) {
-   const fullNameUpper = fullName.toUpperCase();
-   const firstNameUpper = firstName.toUpperCase();
+function getShippingCost(country) {
+  switch (country) {
+    case "China": return `"Shipping to ${country} will cost 100 credits"`;
+      break;
+    
+    case "Chile": return `"Shipping to ${country} will cost 250 credits"`;
+      break; 
+    
+    case "Australia": return `"Shipping to ${country} will cost 170 credits"`;
+      break; 
 
-  console.log (fullNameUpper.includes(firstNameUpper));
+    case "Jamaica": return `"Shipping to ${country} will cost 120 credits"`;
+      break;     
+  
+    default: return `"Sorry, there is no delivery to your country".`;
+  }
 }
 
-
-checkForName("Jason Neis", "Jason");
-checkForName("Jason Neis", "jAsOn");
-checkForName("Jason Neis", "Jacob");
-checkForName("Caty Stars", "Caty");
-checkForName("Caty Stars", "cAtY");
-checkForName("Caty Stars", "Andromeda");
-
+console.log(getShippingCost("Australia")); 
+console.log(getShippingCost("Germany")); 
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden")); 
